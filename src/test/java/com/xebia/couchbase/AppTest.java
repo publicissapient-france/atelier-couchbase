11de27c73d8cebb258be1ea9f6d6739c5fb8ebe3
Antoine MICHAUD <antoinemichaud34@gmail.com>
2014-08-10 18:47:57 +0200

    complete couchbase cluster configuration file + get document

--- src/test/java/com/xebia/couchbase/AppTest.java
+++ src/test/java/com/xebia/couchbase/AppTest.java
@@ -1,13 +1,23 @@
 package com.xebia.couchbase;
 
 
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.Cluster;
+import com.couchbase.client.java.CouchbaseCluster;
+import com.couchbase.client.java.document.JsonDocument;
 import org.junit.Test;
+import rx.Observable;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class AppTest {
 
     @Test
     public void testApp() {
-        assertThat(true).isTrue();
+        final Cluster couchbaseCluster = new CouchbaseCluster("127.0.0.1");
+        final Bucket defaultBucket = couchbaseCluster.openBucket().toBlocking().single();
+        final JsonDocument testJsonDocument = defaultBucket.get("test").toBlocking().single();
+
+        assertThat(testJsonDocument.content().get("cle")).isEqualTo("valeur");
     }
 }
