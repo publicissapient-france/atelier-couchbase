b95216f8794c3d065c60ac2b8ce288006a053419
Antoine MICHAUD <antoinemichaud34@gmail.com>
2014-08-10 15:58:01 +0200

    initial commit

---
+++ src/main/java/com/xebia/couchbase/App.java
@@ -0,0 +1,7 @@
+package com.xebia.couchbase;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/xebia/couchbase/AppTest.java
@@ -0,0 +1,13 @@
+package com.xebia.couchbase;
+
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertThat(true).isTrue();
+    }
+}
