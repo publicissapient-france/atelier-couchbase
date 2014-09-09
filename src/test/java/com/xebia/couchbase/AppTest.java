730a2b44c6a2a1700b4f3967fd19931907dea5a4
Antoine MICHAUD <antoinemichaud34@gmail.com>
2014-09-09 22:37:19 +0200

    implement hello world step

--- src/test/java/com/xebia/couchbase/AppTest.java
+++ src/test/java/com/xebia/couchbase/AppTest.java
@@ -6,18 +6,20 @@ import com.couchbase.client.java.Cluster;
 import com.couchbase.client.java.CouchbaseCluster;
 import com.couchbase.client.java.document.JsonDocument;
 import org.junit.Test;
-import rx.Observable;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
 
-    @Test
-    public void testApp() {
-        final Cluster couchbaseCluster = new CouchbaseCluster("127.0.0.1");
-        final Bucket defaultBucket = couchbaseCluster.openBucket().toBlocking().single();
-        final JsonDocument testJsonDocument = defaultBucket.get("test").toBlocking().single();
+    //TODO change this url to run it on your EC2 instance
+    public static final String COUCHBASE_SERVER_ADDRESS = "127.0.0.1";
 
-        assertThat(testJsonDocument.content().get("cle")).isEqualTo("valeur");
+    @Test
+    public void should_get_hello_world_document() {
+        final Cluster couchbaseCluster = new CouchbaseCluster(COUCHBASE_SERVER_ADDRESS);
+        final Bucket defaultBucket = couchbaseCluster.openBucket().toBlocking().single();
+        final JsonDocument testJsonDocument = defaultBucket.get("hello_couchbase").toBlocking().single();
+
+        assertThat(testJsonDocument.content().get("hello")).isEqualTo("world");
     }
 }
