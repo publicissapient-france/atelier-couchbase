1b920d96e2182987ce7f87c7c473494d300f5dec
Antoine MICHAUD <antoinemichaud34@gmail.com>
2014-09-20 22:33:37 +0200

    Add test for bulk insertion

---
+++ src/main/java/com/xebia/couchbase/batch/UserReaderFromCsv.java
@@ -0,0 +1,49 @@
+package com.xebia.couchbase.batch;
+
+import com.xebia.couchbase.location.AddressBuilder;
+import com.xebia.couchbase.location.City;
+import com.xebia.couchbase.location.Country;
+import com.xebia.couchbase.user.User;
+import com.xebia.couchbase.user.UserBuilder;
+import com.xebia.couchbase.user.UserProfileBuilder;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class UserReaderFromCsv {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserReaderFromCsv.class);
+
+    public static Collection<User> getUsersFrom(String usersFileName) {
+        final ArrayList<User> users = new ArrayList<>();
+        AtomicLong idGenerator = new AtomicLong();
+
+        try {
+            final InputStream inputStream = UserReaderFromCsv.class.getResourceAsStream("/" + usersFileName);
+            final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            final Iterable<CSVRecord> lines = CSVFormat.DEFAULT.parse(inputStreamReader);
+
+            for (CSVRecord line : lines) {
+                users.add(UserBuilder.anUser().withIdentifier(idGenerator.incrementAndGet())
+                        .withUserProfile(UserProfileBuilder.anUserProfile()
+                                .withFirstName(line.get(0))
+                                .withLastName(line.get(1))
+                                .withSummary("Java Developer")
+                                .withAddress(AddressBuilder.anAddress()
+                                        .withCountry(new Country("US"))
+                                        .withCity(new City(line.get(2), Integer.parseInt(line.get(3))))
+                                        .build()).build()).build());
+            }
+        } catch (IOException e) {
+            logger.error(String.format("The file %s couldn't be read.", usersFileName), e);
+        }
+
+        return users;
+    }
+}
--- src/test/java/com/xebia/couchbase/user/UserRepositoryTest.java
+++ src/test/java/com/xebia/couchbase/user/UserRepositoryTest.java
@@ -1,11 +1,22 @@
 package com.xebia.couchbase.user;
 
+import com.xebia.couchbase.batch.UserReaderFromCsv;
 import com.xebia.couchbase.location.AddressBuilder;
 import com.xebia.couchbase.location.City;
 import com.xebia.couchbase.location.Country;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Collection;
+
 public class UserRepositoryTest {
+
+    private UserRepository userRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        userRepository = new UserRepository();
+    }
 
     @Test
     public void should_insert_user_in_database() throws Exception {
@@ -14,7 +25,15 @@ public class UserRepositoryTest {
                 .withAddress(AddressBuilder.anAddress().withCity(new City("Paris", 1_000_000)).withCountry(new Country("France"))
                         .build()).build()).build();
 
-        final UserRepository userRepository = new UserRepository();
         userRepository.saveUser(user);
+    }
+
+    @Test
+    public void should_insert_a_bulk_of_users() throws Exception {
+        final Collection<User> users = UserReaderFromCsv.getUsersFrom("users.csv");
+
+        for (User user : users) {
+            userRepository.saveUser(user);
+        }
     }
 }
