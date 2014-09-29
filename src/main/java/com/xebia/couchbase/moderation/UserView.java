0a5faf20c743d7a6116e0815a2a88cd425d68279
Antoine MICHAUD <antoinemichaud34@gmail.com>
2014-09-30 00:49:32 +0200

    Add precisions on todos

--- src/main/java/com/xebia/couchbase/moderation/UserView.java
+++ src/main/java/com/xebia/couchbase/moderation/UserView.java
@@ -1,37 +1,40 @@
 package com.xebia.couchbase.moderation;
 
 import com.couchbase.client.protocol.views.*;
+import com.google.gson.Gson;
 
 import static com.xebia.couchbase.Configuration.PUBLICOTAURUS_CLIENT;
 
 public class UserView {
 
+    //TODO Exercice 9.1 : Récupérer les vues
     private static final View inactiveUserView = PUBLICOTAURUS_CLIENT.getView("moderator", "inactive_user");
     private static final View activeUserView = PUBLICOTAURUS_CLIENT.getView("moderator", "active_user");
-    //TODO Exercice 10 : Stocker un attribut permettant d'itérer sur les résultats
+    private static final Gson gson = new Gson();
+    //TODO Exercice 10.1 : Stocker un attribut permettant d'itérer sur les résultats
     private Paginator activeUserResponsePaginator;
 
-    //TODO Exercice 10 : Stocker un attribut permettant d'itérer sur les résultats
+    //TODO Exercice 10.2 : Stocker un attribut permettant d'itérer sur les résultats
     public UserView(Paginator activeUserResponsePaginator) {
         this.activeUserResponsePaginator = activeUserResponsePaginator;
     }
 
-    //TODO Exercice 9
+    //TODO Exercice 9.2 : Requêter les vues
     public static ViewResponse getInactiveUsers() {
-        final View inactiveUserView = UserView.inactiveUserView;
         final Query query = new Query();
-        query.setStale(Stale.UPDATE_AFTER);
-        return PUBLICOTAURUS_CLIENT.query(inactiveUserView, query);
+        query.setStale(Stale.FALSE);
+        return PUBLICOTAURUS_CLIENT.query(UserView.inactiveUserView, query);
     }
 
-    //TODO Exercice 10
+    //TODO Exercice 10.3
     public static UserView getPaginatedActiveUsers() {
         final Query query = new Query();
-        query.setStale(Stale.UPDATE_AFTER);
+        query.setStale(Stale.FALSE);
 
         return new UserView(PUBLICOTAURUS_CLIENT.paginatedQuery(activeUserView, query, 100));
     }
 
+    //TODO Exercice 10.4
     public Iterable<ViewRow> nextPage() {
         if (activeUserResponsePaginator.hasNext()) {
             return activeUserResponsePaginator.next();
