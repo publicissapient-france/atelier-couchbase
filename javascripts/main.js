a71192b94f90d37e4a7d5e2eecfb96824eca99ad
Jean-Eudes <Jean-Eudes@users.noreply.github.com>
2014-09-30 22:22:07 +0200

    rename solution to aide

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
   jQuery(".solution").wrap('<div class = "spoiler"></div>')
-  jQuery(".solution").before('<button  type="button" class = "teaser">Afficher la solution</button>')
+  jQuery(".solution").before('<button  type="button" class = "teaser">Afficher une aide</button>')
   jQuery(".solution").hide();
   jQuery(document).on('click', '.teaser', function() {
     jQuery(this).next(".solution").slideToggle(500);
