323d596c5bef5d7ebd97fece10a1700049efa6aa
Antoine MICHAUD <antoinemichaud34@gmail.com>
2014-10-24 22:17:42 +0200

    Inactive users view access

--- src/main/java/com/xebia/couchbase/moderation/UserView.java
+++ src/main/java/com/xebia/couchbase/moderation/UserView.java
@@ -1,48 +1,14 @@
 package com.xebia.couchbase.moderation;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
+import com.couchbase.client.java.view.Stale;
+import com.couchbase.client.java.view.ViewQuery;
+import com.couchbase.client.java.view.ViewResult;
+import com.xebia.couchbase.Configuration;
 
 public class UserView {
-    private String userId;
-    private String userName;
-    private String cityName;
-    private boolean active;
 
-    public UserView(String userId, String userName, String cityName, boolean active) {
-        this.userId = userId;
-        this.userName = userName;
-        this.cityName = cityName;
-        this.active = active;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (obj == this) {
-            return true;
-        }
-        if (obj.getClass() != getClass()) {
-            return false;
-        }
-        UserView rhs = (UserView) obj;
-        return new EqualsBuilder()
-                .append(this.userId, rhs.userId)
-                .append(this.userName, rhs.userName)
-                .append(this.cityName, rhs.cityName)
-                .append(this.active, rhs.active)
-                .isEquals();
-    }
-
-    @Override
-    public int hashCode() {
-        return new HashCodeBuilder()
-                .append(userId)
-                .append(userName)
-                .append(cityName)
-                .append(active)
-                .toHashCode();
+    //TODO Exercice 9.2 : Requêter les vues
+    public static ViewResult getInactiveUsers() {
+        return Configuration.publicotaurusBucket().query(ViewQuery.from("moderator", "inactive_user").stale(Stale.FALSE));
     }
 }
--- src/test/java/com/xebia/couchbase/moderation/UserViewRepositoryTest.java
+++ src/test/java/com/xebia/couchbase/moderation/UserViewRepositoryTest.java
@@ -1,13 +1,13 @@
 package com.xebia.couchbase.moderation;
 
-import com.couchbase.client.java.view.Stale;
-import com.couchbase.client.java.view.ViewQuery;
+import com.couchbase.client.java.document.json.JsonObject;
 import com.couchbase.client.java.view.ViewResult;
-import com.xebia.couchbase.Configuration;
+import com.couchbase.client.java.view.ViewRow;
+import com.google.common.collect.ImmutableMap;
 import org.junit.Test;
 
-import static com.couchbase.client.java.query.Select.select;
-import static com.couchbase.client.java.query.dsl.Expression.x;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserViewRepositoryTest {
@@ -15,35 +15,52 @@ public class UserViewRepositoryTest {
     public void should_retrieve_user_from_moderator_point_of_view() throws Exception {
         // Given
         // When
-        final ViewResult activeUsers =
-                Configuration.publicotaurusBucket().query(ViewQuery.from("moderator", "inactive_user").stale(Stale.FALSE));
+        final ViewResult inactiveUsers = UserView.getInactiveUsers();
 
         // Then
-        assertThat(activeUsers.allRows().get(0)).isNotNull();
+        final ViewRow userViewRow = inactiveUsers.allRows().get(0);
+        assertThat(userViewRow).isNotNull();
+        assertThat(userViewRow.key()).isEqualTo("VINCENT");
+
+        Map<String, Object> expectedJsonMap = ImmutableMap.<String, Object>builder()
+                .put("userId", "user::adrian_vincent")
+                .put("userName", "ADRIAN VINCENT")
+                .put("cityName", "San Diego")
+                .put("active", false)
+                .build();
+        assertThat(((JsonObject)userViewRow.value()).toMap()).isEqualTo(expectedJsonMap);
     }
 
     @Test
-    public void should_retrieve_most_present_cities() throws Exception {
-        // Given
-
-        // When
-        Configuration.publicotaurusBucket()
-                .async()
-                .query(
-                        select("*").from(Configuration.PUBLICOTAURUS_BUCKET_NAME).where(x("active").is(x(false))))
-        .doOnNext(result -> {
-            if (!result.success()) {
-                System.err.println(result.error());
-            }
-        });
-
-        // Then
+    //Exercice 10
+    public void should_paginate() throws Exception {
 
     }
 
-//    public void disableAnUser(String userId) throws IOException {
+    //    public void disableAnUser(String userId) throws IOException {
 //        final UserWithCas userWithCas = userRepository.findUserWithCas(userId);
 //        userWithCas.getUser().setActive(false);
 //        userRepository.updateUser(userId, userWithCas.getCasId(), userWithCas.getUser());
 //    }
+
+//    @Test
+//    public void should_retrieve_most_present_cities() throws Exception {
+//        // Given
+//
+//        // When
+//        Configuration.publicotaurusBucket()
+//                .async()
+//                .query(
+//                        select("*").from(Configuration.PUBLICOTAURUS_BUCKET_NAME).where(x("active").is(x(false))))
+//        .doOnNext(result -> {
+//            if (!result.success()) {
+//                System.err.println(result.error());
+//            }
+//        });
+//
+//        // Then
+//
+//    }
+
+
 }
